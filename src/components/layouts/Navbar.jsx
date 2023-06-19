e4685aa9a738a7903fcddd9a9672757ddb3bed6d
saadabban76 <saadabban76@gmail.com>
2023-06-19 21:30:31 +0530

    added setupProxy.js file in root folder

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { SiConstruct3 } from "react-icons/si";
 import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import { FaWallet } from "react-icons/fa";
