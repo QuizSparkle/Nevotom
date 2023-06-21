d46ba578552fe0ab4c42f66e992315816f7a9070
saadabban76 <saadabban76@gmail.com>
2023-06-21 20:24:20 +0530

    check

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import { FaWallet } from "react-icons/fa";
