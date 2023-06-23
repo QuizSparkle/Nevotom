615c636ea105745ef80a5468268e63e39a797621
osprey222 <ospray0222@gmail.com>
2023-06-23 14:19:17 +0900

    design cart modal

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Basket from './components/Home/Basket';
 import Pick from './components/Home/Pick';
 import Navbar from './components/NavBar';
 import ShopDetail from './components/ShopDetail';
+import ShoppingCartModal from './components/ShoppingCartModal';
 
 const App: FC = () => {
   const AppLayout = () => (
@@ -29,6 +30,7 @@ const App: FC = () => {
           <Route path="*" element={<Navigate to="/" />} />
           <Route path='/' element={<AppLayout />} />
           <Route path='/detail' element={<ShopDetail />} />
+          <Route path='/shoppingCartModal' element={<ShoppingCartModal handleClose='sdfsdf' isShow={true} goodsCount={12} />} />
         </Routes>
         </Layout>
       </BrowserRouter>
