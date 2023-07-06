4d1aaa45337bfa6c38a9eb620c77705d456ab605
MMtis <marouane.khalifi@gmail.com>
2023-07-06 05:05:37 +0200

    Dockerfile

--- src/components/sellProduct/SellProduct.tsx
+++ src/components/sellProduct/SellProduct.tsx
@@ -66,7 +66,7 @@ const SellProduct = () => {
   
       // Make the API request to create the item
       axios
-        .post("https://5uu62iqlpe.execute-api.us-west-2.amazonaws.com/api/items/create/", formData, {
+        .post("https://0tp092loq8.execute-api.us-west-2.amazonaws.com/api/items/create/", formData, {
           headers: {
             "Content-Type": "multipart/form-data",
           },
