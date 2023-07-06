3d13053facac9fd6714671f1a3723b66c7047a08
MMtis <marouane.khalifi@gmail.com>
2023-07-07 00:20:26 +0200

    Dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "sslserver",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -41,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "main",
-    "sslserver",
 ]
 
 MIDDLEWARE = [
