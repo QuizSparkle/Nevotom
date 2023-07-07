ad6d4756eae48923e0ba0d41986e083de027490e
MMtis <marouane.khalifi@gmail.com>
2023-07-07 17:48:28 +0200

    Dockerfile

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -51,4 +51,5 @@ urlpatterns = [
         UpdateOrderAPIView.as_view(),
         name="update_order",
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
